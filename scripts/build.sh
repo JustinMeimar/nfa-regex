34ff9a2614987eef1360e01a240ebba1cd09844c
Justin Meimar <j.meimar111@gmail.com>
2023-04-18 10:30:41 -0600

    WASM

---
+++ scripts/build.sh
@@ -0,0 +1,1 @@
+emcmake cmake .. -DCMAKE_C_COMPILER=emcc -DCMAKE_CXX_COMPILER=em++ -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=~/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DEMSCRIPTEN=1
