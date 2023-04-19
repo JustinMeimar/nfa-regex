e93a292bc07af466ba2d2d23d5e74df3b26bdfa8
Justin Meimar <j.meimar111@gmail.com>
2023-04-19 12:11:02 -0600

    wrapping nfa with emscripten bindings

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,1 @@
-emcmake cmake .. -DCMAKE_C_COMPILER=emcc -DCMAKE_CXX_COMPILER=em++ -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=~/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DEMSCRIPTEN=1
+emcmake cmake .. -DCMAKE_C_COMPILER=emcc -DCMAKE_CXX_COMPILER=em++ -DEMSCRIPTEN=1 -DCMAKE_TOOLCHAIN_FILE=~/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DEMSCRIPTEN=1 -DCOMPILE_WEB=1
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
-#pragma once
 #include <iostream>
 #include <memory>
 #include <vector>
-
+#ifdef COMPILE_WEB 
+#include <emscripten/bind.h> 
+#endif
 #include "Node.h"
 #include "Parser.h"
 #include "NFA.h"
 #include "Visitor.h"
+
+struct Point {
+    int x;
+    int y;
+};
+
+std::shared_ptr<NFA> run_regex(std::string &regex, std::string &string);
 
 int main(int argc, char** argv) {
 
@@ -22,6 +30,14 @@ int main(int argc, char** argv) {
     } if (argc == 3) {
         string = argv[2]; //string to run on RE 
     } 
+    run_regex(regex, string);
+
+    return 0;
+}
+
+std::shared_ptr<NFA> run_regex(std::string &regex, std::string &string) {
+
+    auto regexParser = std::make_shared<Parser>();   
 
     regexParser->readStream(regex);
     regexParser->insertImplicitConcatTokens();
@@ -32,13 +48,27 @@ int main(int argc, char** argv) {
     visitor->visit(root);
 
     std::shared_ptr<NFA> nfa = visitor->nfa;
+    
     nfa->execute(nfa->startState, string, 0);
-
-    if (nfa->accept) {
+     if (nfa->accept) {
         std::cout << "Accept String :D" << std::endl;
     } else {
         std::cout << "Reject String :(" << std::endl;
-    }
-
-    return 0;
+    }   
+    return nfa;
 }
+
+#ifdef COMPILE_WEB
+EMSCRIPTEN_BINDINGS(my_module) {
+    emscripten::class_<Point>("Point")
+    .constructor<>()
+    .property("x", &Point::x)
+    .property("y", &Point::y);
+
+    // emscripten::class_<Parser>("Parser")
+    // .constructor<>
+    // .function()  // sta
+    // .property() // static member 
+
+}
+#endif
