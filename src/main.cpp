ac6bd4c2f90e963cdbeee547ce96f1beb27f6d24
Justin Meimar <j.meimar111@gmail.com>
2023-04-20 12:59:06 -0600

    reduce bindings to minimal interface

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,7 @@
 #include "NFA.h"
 #include "Visitor.h"
 
-struct Point {
-    int x;
-    int y;
-};
-
-std::shared_ptr<NFA> run_regex(std::string &regex, std::string &string);
+NFA *createNFA(const std::string &regex);
 
 int main(int argc, char** argv) {
 
@@ -29,16 +24,25 @@ int main(int argc, char** argv) {
         regex = argv[1]; //regex specification
     } if (argc == 3) {
         string = argv[2]; //string to run on RE 
-    } 
-    run_regex(regex, string);
+    }
 
+    NFA *nfa = createNFA(regex);
+    nfa->execute(nfa->startState, string, 0);
+    
+    if (nfa->accept) {
+        std::cout << "Accept String :D" << std::endl;
+    } else {
+        std::cout << "Reject String :(" << std::endl;
+    }   
+    
     return 0;
 }
 
-std::shared_ptr<NFA> run_regex(std::string &regex, std::string &string) {
+
+// NFA FUNCTIONALITY
+NFA *createNFA(const std::string &regex) {
 
     auto regexParser = std::make_shared<Parser>();   
-
     regexParser->readStream(regex);
     regexParser->insertImplicitConcatTokens();
     regexParser->regex();
@@ -47,51 +51,72 @@ std::shared_ptr<NFA> run_regex(std::string &regex, std::string &string) {
     std::shared_ptr<Visitor> visitor = std::make_shared<Visitor>(); 
     visitor->visit(root);
 
-    std::shared_ptr<NFA> nfa = visitor->nfa;
-    
+    return visitor->nfa.get();
+}
+
+void executeNFA(NFA *nfa, const std::string &string) {
     nfa->execute(nfa->startState, string, 0);
-     if (nfa->accept) {
-        std::cout << "Accept String :D" << std::endl;
-    } else {
-        std::cout << "Reject String :(" << std::endl;
-    }   
-    return nfa;
+}
+
+// WRAPPERS
+std::vector<uint32_t> getStatesWrapper(NFA *nfa) {
+    std::vector<uint32_t> states;
+    for (auto state : nfa->states) {
+        states.push_back( (uint32_t) state.get());
+    }
+    return states;
+}
+
+std::vector<uint32_t> getAcceptStatesWrapper(NFA *nfa) {
+    std::vector<uint32_t> acceptStates;
+    for (auto state : nfa->acceptStates) {
+        acceptStates.push_back( (uint32_t) state.get());
+    }
+    return acceptStates;
+}
+
+bool getDidAcceptWrapper(NFA *nfa) {
+    return nfa->accept;
+}
+
+uint32_t getStartStateWrapper(NFA *nfa) {
+    return (uint32_t) nfa->startState.get();
+}
+
+// std::vector<std::tuple<int, int, char>> getTransitionTableWrapper(NFA *nfa) { 
+
+//     std::vector<std::tuple<uint32_t, uint32_t, char>> transition_table;
+
+//     for (auto it = nfa->transition_table.begin(); it != nfa->transition_table.end(); it++) {
+//         std::tuple<uint32_t, uint32_t, char> transition_tup;
+//         std::set transition_tup
+        
+//     }
+// }
+
+EMSCRIPTEN_BINDINGS(my_module) {  
+    emscripten::class_<NFA>("NFA");
+
+    emscripten::function("createNFA", &createNFA, emscripten::allow_raw_pointers()); 
+    emscripten::function("executeNFA", &executeNFA, emscripten::allow_raw_pointers()); 
+    emscripten::function("getDidAccept", &getDidAcceptWrapper, emscripten::allow_raw_pointers()); 
+      
+    // emscripten::function("createNFALeafFactory", &createNFALeafFactory); 
+    // emscripten::function("createNFABinaryOpFactory", &createNFABinaryOpFactory); 
+    // emscripten::function("createNFAUnaryOpFactory", &createNFAUnaryOpFactory); 
+ 
+    // emscripten::class_<Point>("Point")
+    // .constructor<>()
+    // .property("x", &Point::x)
+    // .property("y", &Point::
+    // emscripten::enum_<StateType>("StateType")
+    //     .value("ACCEPT", StateType::ACCEPT)
+    //     .value("REJECT", StateType::REJECT);
+
+    // emscripten::class_<State>("State")
+    //     .smart_ptr_constructor("State", &std::make_shared<State, StateType>)
+    //     .property("type", &State::type);
 }
 
 #ifdef COMPILE_WEB
-EMSCRIPTEN_BINDINGS(my_module) {
-    emscripten::class_<Point>("Point")
-    .constructor<>()
-    .property("x", &Point::x)
-    .property("y", &Point::y);
-
-    emscripten::enum_<StateType>("StateType")
-        .value("ACCEPT", StateType::ACCEPT)
-        .value("REJECT", StateType::REJECT);
-
-    emscripten::class_<State>("State")
-        .smart_ptr_constructor("State", &std::make_shared<State, StateType>)
-        .property("type", &State::type);
-
-    /*
-    //Define some custom types
-    emscripten::type<StateSet>();
-    emscripten::type<TransitionTuple>();
-    emscripten::type<TransitionTable>();
-
-    //Define the NFA class
-    emscripten::class_<NFA>("Nfa") 
-        .constructor<>() 
-        .function() // interface to get states
-        .function() // interface to get transitions
-        .function() // interface to execute the NFA and mutate the 
-        .function() // interface to get the accept state of the machine
-
-    */
-    // emscripten::class_<Parser>("Parser")
-    // .constructor<>
-    // .function()  // sta
-    // .property() // static member 
-
-}
 #endif
